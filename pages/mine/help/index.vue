26722dbf562dd7e18a0ec13f6ddfc298acabde67
qqz <1684182726@qq.com>
2024-07-10 09:07:23 +0800

    个人页面修改

--- pages/mine/help/index.vue
+++ pages/mine/help/index.vue
@@ -16,97 +16,97 @@
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        list: [{
-            icon: 'iconfont icon-github',
-            title: '若依问题',
-            childList: [{
-              title: '若依开源吗？',
-              content: '开源'
-            }, {
-              title: '若依可以商用吗？',
-              content: '可以'
-            }, {
-              title: '若依官网地址多少？',
-              content: 'http://ruoyi.vip'
-            }, {
-              title: '若依文档地址多少？',
-              content: 'http://doc.ruoyi.vip'
-            }]
-          },
-          {
-            icon: 'iconfont icon-help',
-            title: '其他问题',
-            childList: [{
-              title: '如何退出登录？',
-              content: '请点击[我的] - [应用设置] - [退出登录]即可退出登录',
-            }, {
-              title: '如何修改用户头像？',
-              content: '请点击[我的] - [选择头像] - [点击提交]即可更换用户头像',
-            }, {
-              title: '如何修改登录密码？',
-              content: '请点击[我的] - [应用设置] - [修改密码]即可修改登录密码',
-            }]
-          }
-        ]
-      }
-    },
-    methods: {
-      handleText(item) {
-        this.$tab.navigateTo(`/pages/common/textview/index?title=${item.title}&content=${item.content}`)
-      }
+export default {
+  data() {
+    return {
+      list: [
+        {
+          icon: 'iconfont icon-github',
+          title: '本软件开源吗',
+          childList: [
+            { title: '本软件开源吗？', content: '开源' },
+            { title: '本软件可以商用吗？', content: '可以' },
+            { title: '本软件官网地址多少？', content: 'https://github.com/blhqwjs/2024-' },
+            { title: '本软件文档地址多少？', content: 'https://github.com/blhqwjs/2024-' }
+          ]
+        },
+        {
+          icon: 'iconfont icon-help',
+          title: '其他问题',
+          childList: [
+            { title: '如何退出登录？', content: '请点击[我的] - [应用设置] - [退出登录]即可退出登录' },
+            { title: '如何修改用户头像？', content: '请点击[我的] - [选择头像] - [点击提交]即可更换用户头像' },
+            { title: '如何修改登录密码？', content: '请点击[我的] - [应用设置] - [修改密码]即可修改登录密码' }
+          ]
+        }
+      ]
+    };
+  },
+  methods: {
+    handleText(item) {
+      this.$tab.navigateTo(`/pages/common/textview/index?title=${item.title}&content=${item.content}`);
     }
   }
+};
 </script>
 
 <style lang="scss" scoped>
-  page {
-    background-color: #f8f8f8;
-  }
+page {
+  background-color: #f5f5f5;
+}
 
-  .help-container {
-    margin-bottom: 100rpx;
-    padding: 30rpx;
-  }
+.help-container {
+  margin-bottom: 100rpx;
+  padding: 30rpx;
+}
 
-  .list-title {
-    margin-bottom: 30rpx;
-  }
+.list-title {
+  margin-bottom: 20rpx;
+  background: #fff;
+  padding: 20rpx;
+  border-radius: 16rpx;
+  box-shadow: 0 2px 8rpx rgba(0, 0, 0, 0.1);
+}
 
-  .childList {
-    background: #ffffff;
-    box-shadow: 0px 0px 10rpx rgba(193, 193, 193, 0.2);
-    border-radius: 16rpx;
-    margin-top: 10rpx;
-  }
+.text-title {
+  display: flex;
+  align-items: center;
+  font-size: 32rpx;
+  font-weight: bold;
+  color: #303133;
+  padding: 10rpx 0;
 
-  .line {
-    width: 100%;
-    height: 1rpx;
-    background-color: #F5F5F5;
+  .iconfont {
+    font-size: 36rpx;
+    margin-right: 15rpx;
+    color: #409EFF;
   }
+}
 
-  .text-title {
-    color: #303133;
-    font-size: 32rpx;
-    font-weight: bold;
-    margin-left: 10rpx;
+.childList {
+  margin-top: 10rpx;
+}
 
-    .iconfont {
-      font-size: 16px;
-      margin-right: 10rpx;
-    }
+.text-item {
+  font-size: 28rpx;
+  color: #606266;
+  padding: 20rpx;
+}
+
+.line {
+  width: 100%;
+  height: 1rpx;
+  background-color: #ebeef5;
+}
+
+.question {
+  transition: background-color 0.3s, color 0.3s;
+  border-radius: 12rpx;
+  padding: 20rpx;
+
+  &:hover {
+    background-color: #e6f7ff;
+    color: #409EFF;
   }
-
-  .text-item {
-    font-size: 28rpx;
-    padding: 24rpx;
-  }
-
-  .question {
-    color: #606266;
-    font-size: 28rpx;
-  }
+}
 </style>
--- pages/mine/setting/index.vue
+++ pages/mine/setting/index.vue
@@ -1,78 +1,99 @@
 <template>
-  <view class="setting-container" :style="{height: `${windowHeight}px`}">
+  <view class="setting-container" :style="{ height: windowHeight + 'px' }">
     <view class="menu-list">
       <view class="list-cell list-cell-arrow" @click="handleToPwd">
         <view class="menu-item-box">
           <view class="iconfont icon-password menu-icon"></view>
-          <view>修改密码</view>
+          <view class="menu-text">修改密码</view>
         </view>
       </view>
       <view class="list-cell list-cell-arrow" @click="handleToUpgrade">
         <view class="menu-item-box">
           <view class="iconfont icon-refresh menu-icon"></view>
-          <view>检查更新</view>
+          <view class="menu-text">检查更新</view>
         </view>
       </view>
       <view class="list-cell list-cell-arrow" @click="handleCleanTmp">
         <view class="menu-item-box">
           <view class="iconfont icon-clean menu-icon"></view>
-          <view>清理缓存</view>
+          <view class="menu-text">清理缓存</view>
         </view>
       </view>
     </view>
     <view class="cu-list menu">
-      <view class="cu-item item-box">
-        <view class="content text-center" @click="handleLogout">
-          <text class="text-black">退出登录</text>
-        </view>
+      <view class="cu-item item-box" @click="handleLogout">
+        <text class="text-black">退出登录</text>
       </view>
     </view>
   </view>
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        windowHeight: uni.getSystemInfoSync().windowHeight
-      }
+export default {
+  data() {
+    return {
+      windowHeight: uni.getSystemInfoSync().windowHeight
+    }
+  },
+  methods: {
+    handleToPwd() {
+      this.$tab.navigateTo('/pages/mine/pwd/index')
     },
-    methods: {
-      handleToPwd() {
-        this.$tab.navigateTo('/pages/mine/pwd/index')
-      },
-      handleToUpgrade() {
-        this.$modal.showToast('模块建设中~')
-      },
-      handleCleanTmp() {
-        this.$modal.showToast('模块建设中~')
-      },
-      handleLogout() {
-        this.$modal.confirm('确定注销并退出系统吗？').then(() => {
-          this.$store.dispatch('LogOut').then(() => {
-            this.$tab.reLaunch('/pages/index')
-          })
+    handleToUpgrade() {
+      this.$modal.showToast('功能正在建设中~')
+    },
+    handleCleanTmp() {
+      this.$modal.showToast('功能正在建设中~')
+    },
+    handleLogout() {
+      this.$modal.confirm('确定注销并退出系统吗？').then(() => {
+        this.$store.dispatch('LogOut').then(() => {
+          this.$tab.reLaunch('/pages/index')
         })
-      }
+      })
     }
   }
+}
 </script>
 
 <style lang="scss" scoped>
-  .page {
-    background-color: #f8f8f8;
-  }
+.setting-container {
+  background-color: #f8f8f8;
+}
 
-  .item-box {
-    background-color: #FFFFFF;
-    margin: 30rpx;
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    align-items: center;
-    padding: 10rpx;
-    border-radius: 8rpx;
-    color: #303133;
-    font-size: 32rpx;
-  }
+.menu-list {
+  margin-top: 20px;
+}
+
+.list-cell {
+  padding: 20rpx;
+  border-bottom: 1px solid #eee;
+}
+
+.menu-item-box {
+  display: flex;
+  align-items: center;
+}
+
+.menu-icon {
+  font-size: 36rpx;
+  margin-right: 20rpx;
+}
+
+.menu-text {
+  font-size: 32rpx;
+  color: #303133;
+}
+
+.item-box {
+  background-color: #fff;
+  margin: 30rpx;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 20rpx;
+  border-radius: 8rpx;
+  font-size: 32rpx;
+  color: #303133;
+}
 </style>
