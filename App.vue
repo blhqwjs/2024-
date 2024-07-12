c61ade082f748d642fde4b0ca89699e869aef89f
wujunyu <2662432484@qq.com>
2024-07-12 10:00:43 +0800

    观看历史和我的收藏的前端页面展示和逻辑实现

--- App.vue
+++ App.vue
@@ -5,8 +5,7 @@
 
   export default {
 	  globalData: {
-	  	Authorization:
-		""
+	  	Authorization: "Bearer eyJhbGciOiJIUzUxMiJ9.eyJsb2dpbl91c2VyX2tleSI6ImJmMjg5MjcwLWQ5N2EtNDNmOC1hZTUwLTQ3MmI2ZDE0ZjE5NSJ9.FsUVDTVFlDcxU-sCr2xBr3VDeyP4rmhfBzoURJbe4zNORMl3ZNggyTpnMZqJBQk-oqueiQRkg55ObXC2FLOr6g"
 	  },
 	  
     onLaunch: function() {
--- pages/work/index.vue
+++ pages/work/index.vue
@@ -1,183 +1,328 @@
 <template>
-  <view class="work-container">
-    <!-- 轮播图 -->
-    <uni-swiper-dot class="uni-swiper-dot-box" :info="data" :current="current" field="content">
-      <swiper class="swiper-box" :current="swiperDotIndex" @change="changeSwiper">
-        <swiper-item v-for="(item, index) in data" :key="index">
-          <view class="swiper-item" @click="clickBannerItem(item)">
-            <image :src="item.image" mode="aspectFill" :draggable="false" />
-          </view>
-        </swiper-item>
-      </swiper>
-    </uni-swiper-dot>
-
-    <!-- 宫格组件 -->
-    <uni-section title="系统管理" type="line"></uni-section>
-    <view class="grid-body">
-      <uni-grid :column="4" :showBorder="false" @change="changeGrid">
-        <uni-grid-item>
-          <view class="grid-item-box">
-            <uni-icons type="person-filled" size="30"></uni-icons>
-            <text class="text">用户管理</text>
-          </view>
-        </uni-grid-item>
-        <uni-grid-item>
-          <view class="grid-item-box">
-            <uni-icons type="staff-filled" size="30"></uni-icons>
-            <text class="text">角色管理</text>
-          </view>
-        </uni-grid-item>
-        <uni-grid-item>
-          <view class="grid-item-box">
-            <uni-icons type="color" size="30"></uni-icons>
-            <text class="text">菜单管理</text>
-          </view>
-        </uni-grid-item>
-        <uni-grid-item>
-          <view class="grid-item-box">
-            <uni-icons type="settings-filled" size="30"></uni-icons>
-            <text class="text">部门管理</text>
-          </view>
-        </uni-grid-item>
-        <uni-grid-item>
-          <view class="grid-item-box">
-            <uni-icons type="heart-filled" size="30"></uni-icons>
-            <text class="text">岗位管理</text>
-          </view>
-        </uni-grid-item>
-        <uni-grid-item>
-          <view class="grid-item-box">
-            <uni-icons type="bars" size="30"></uni-icons>
-            <text class="text">字典管理</text>
-          </view>
-        </uni-grid-item>
-        <uni-grid-item>
-          <view class="grid-item-box">
-            <uni-icons type="gear-filled" size="30"></uni-icons>
-            <text class="text">参数设置</text>
-          </view>
-        </uni-grid-item>
-        <uni-grid-item>
-          <view class="grid-item-box">
-            <uni-icons type="chat-filled" size="30"></uni-icons>
-            <text class="text">通知公告</text>
-          </view>
-        </uni-grid-item>
-        <uni-grid-item>
-          <view class="grid-item-box">
-            <uni-icons type="wallet-filled" size="30"></uni-icons>
-            <text class="text">日志管理</text>
-          </view>
-        </uni-grid-item>
-      </uni-grid>
-    </view>
-  </view>
+	<view id="index">
+		
+		<view class="header-box">
+			<view class="tabs-box">
+				<view class="one-nav" :class="currentSwiperIndex === 0 ? 'nav-actived' : '' " @tap="swiperChange(0)" @click="getRecordsList()">观看历史</view>
+				<view class="one-nav" :class="currentSwiperIndex === 1 ? 'nav-actived' : '' " @tap="swiperChange(1)" @click="getNews()">我的收藏</view>
+			</view>
+		</view>
+	
+		<swiper class="swiper-box" style="height: 1000rpx" :current="currentSwiperIndex">
+			<swiper-item class="swiper-item">
+			    <record-view v-show="isSearchResultsVisible2" class="record" record-y>
+			        <view class="record_class">
+			            <view class="record_item" v-for="(item, index) in recordList" :key="index">
+			                <view class="createTime">{{item.createTime}}</view>
+			                <image class="image" :src="item.image"></image>
+			                <view class="text">{{item.text}}</view>
+			            </view>
+			        </view>
+			    </record-view>
+			</swiper-item>
+			<swiper-item class="swiper-item">
+					<collect-view v-show="isSearchResultsVisible2" class="collect" collect-y>
+						<view class="collect_class">
+							<view class="collect_item" v-for="(item, index) in collectList" :key="index" >
+								<image class="image" :src="item.image"></image>
+								<text class="text">{{item.text}}</text>
+								<view class="info">
+									<text class="free">{{item.free}}</text>
+									<text class="watchTime">{{item.watchTime}}</text>
+								</view>
+							</view>
+						</view>
+					</collect-view>
+			</swiper-item>
+		</swiper>
+	
+	</view>
+	
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        current: 0,
-        swiperDotIndex: 0,
-        data: [{
-            image: '/static/images/banner/banner01.jpg'
-          },
-          {
-            image: '/static/images/banner/banner02.jpg'
-          },
-          {
-            image: '/static/images/banner/banner03.jpg'
-          }
-        ]
-      }
-    },
-    methods: {
-      clickBannerItem(item) {
-        console.info(item)
-      },
-      changeSwiper(e) {
-        this.current = e.detail.current
-      },
-      changeGrid(e) {
-        this.$modal.showToast('模块建设中~')
-      }
-    }
-  }
+import { getRecords,listRecords } from '@/api/summer/records.js'
+import { getSummer_Class } from '@/api/summer/summer_Class.js'
+import { listChapter } from '@/api/summer/chapter.js'
+	
+	export default{
+		
+		data(){
+			return{
+				// 当前 观看历史 我的收藏 滑动位置
+				currentSwiperIndex: 0,
+				recordList:[],
+				collectList:[],
+				isSearchResultsVisible2: true,
+			
+				query:{
+					pageSize:1000,
+					pageNum:1,
+				}
+			}
+		},
+		onLoad(){
+			this.getRecordsList()
+		},
+		methods: {
+			swiperChange(index){
+				this.currentSwiperIndex = index
+			},
+			
+			
+			getRecordsList(){
+				const record = {
+					user_id : "2"
+				}
+				
+				//获取观看历史
+				listRecords(record).then(res => {
+					//清空recordList
+					this.recordList = [];
+					
+					//使用 Set 来确保 class_id 不重复
+					const classIdList = new Set();
+					console.log(res.rows)
+					//获取课程id
+					res.rows.forEach(item =>{
+						classIdList.add(item.class_id)
+						
+					});
+					console.log(classIdList);
+					
+					// 转换为数组并按顺序提取 creat_time 和 class_id
+					const records = [...classIdList].map(classId => {
+						const item = res.rows.find(record => record.class_id === classId);
+						return {
+							createTime: item?item.creat_time : null ,
+							classId: classId,
+						};
+					})
+					console.log(records)
+					//观看历史所需对象
+					
+					records.forEach(item =>{
+						getSummer_Class(item.classId).then(res =>{
+							this.recordList.push({
+								classId: item.classId,
+								createTime: item.createTime,
+								image: res.data.file_url,
+								text: res.data.name
+							});
+						})
+					})
+					console.log(this.recordList)
+					
+				})
+				.catch(error => {
+				// 处理错误
+				console.error(error);
+				});
+			},
+			
+			getNews(){
+				
+				//清空collectList
+				this.collectList = [];
+				
+				const record = {
+					user_id : "2"
+				}
+				//获取我的收藏	
+				listRecords(record).then(res => {
+				// 处理返回的数据
+				// console.log(res)
+				//获取我收藏了的课程编号
+				let classIds = [];
+				res.rows.forEach(res =>{
+					// console.log(res)
+					if(res.type === 2 || res.type === 3){
+						classIds.push(res.class_id)
+					}
+				})
+				console.log(classIds)
+				
+				classIds.forEach(ids =>{
+					getSummer_Class(ids).then(res =>{
+						console.log(res.data)
+						
+						listChapter(this.query).then(chapters =>{
+							let sum = 0;
+							console.log(chapters)
+							chapters.rows.forEach(chapter =>{
+								if(chapter.class_id === ids){
+									sum += chapter.study_num;
+									
+								}
+							})
+						this.collectList.push({
+							classId: ids,
+							image: res.data.file_url,
+							test: res.data.name,
+							free: res.data.tag_id,
+							watchTime: sum,
+						})
+						
+							
+						})
+						
+						
+					})
+				})
+				console.log(this.collectList)
+				
+				}).catch(error => {
+				// 处理错误
+				console.error(error);
+				});
+			}
+
+			
+		},
+	}
 </script>
 
-<style lang="scss">
-  /* #ifndef APP-NVUE */
-  page {
-    display: flex;
-    flex-direction: column;
-    box-sizing: border-box;
-    background-color: #fff;
-    min-height: 100%;
-    height: auto;
-  }
-
-  view {
-    font-size: 14px;
-    line-height: inherit;
-  }
-
-  /* #endif */
-
-  .text {
-    text-align: center;
-    font-size: 26rpx;
-    margin-top: 10rpx;
-  }
-
-  .grid-item-box {
-    flex: 1;
-    /* #ifndef APP-NVUE */
-    display: flex;
-    /* #endif */
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    padding: 15px 0;
-  }
-
-  .uni-margin-wrap {
-    width: 690rpx;
-    width: 100%;
-    ;
-  }
-
-  .swiper {
-    height: 300rpx;
-  }
-
-  .swiper-box {
-    height: 150px;
-  }
-
-  .swiper-item {
-    /* #ifndef APP-NVUE */
-    display: flex;
-    /* #endif */
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    color: #fff;
-    height: 300rpx;
-    line-height: 300rpx;
-  }
-
-  @media screen and (min-width: 500px) {
-    .uni-swiper-dot-box {
-      width: 400px;
-      /* #ifndef APP-NVUE */
-      margin: 0 auto;
-      /* #endif */
-      margin-top: 8px;
-    }
-
-    .image {
-      width: 100%;
-    }
-  }
+<style lang="scss" scoped>
+	#sns {
+		background-color: #f1f1f1;
+	}
+	
+	// 推荐、咨询 按钮样式
+	.tabs-box {
+		display: flex;
+		flex-direction: row;
+		justify-content: center;
+		width: 750upx;
+	
+		.one-nav {
+			width: 50%;
+			display: inline-block;
+			font-size: 30rpx;
+			line-height: 40rpx;
+			text-align: center;
+			padding: 44rpx 16rpx;
+			box-sizing: border-box;
+	
+			&.nav-actived {
+				color: #0050FF;
+			}
+		}
+	}
+	
+	
+	//静态数据展示样式
+	.header-box{
+		position: relative;
+		left: 0;
+		height: 100upx;
+		background-color: #f1f1f1;
+		z-index: 1;
+	}
+	
+	.swiper-box{
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		background: linear-gradient(#E1E6F5, #FFFFFF, #c0bbff);
+		height: 100%;
+		
+		.swiper-item{
+			
+			.collect {
+				height: 100%;
+				width: 730rpx;
+				margin-top: 30rpx;
+			
+				.collect_class {
+					display: flex;
+					flex-wrap: wrap;
+					justify-content: space-between;
+			
+					.collect_item {
+						display: flex;
+						flex-direction: column;
+						margin-top: 20rpx;
+						align-items: center;
+						width: 48%;
+						height: auto;
+						margin-bottom: 15rpx;
+			
+						.image {
+							width: 100%;
+							height: 200rpx;
+						}
+			
+						.text {
+							margin-top: 10rpx;
+							font-size: 30rpx;
+							text-align: center;
+							width: 100%;
+						}
+			
+						.info {
+							display: flex;
+							justify-content: space-between;
+							width: 100%;
+							padding: 0 10rpx;
+							margin-top: 10rpx;
+			
+							.free,
+							.watchTime {
+								font-size: 15px;
+							}
+						}
+					}
+				}
+			}
+			.record {
+				height: 100%;
+				width: 730rpx;
+				margin-top: 3rpx;
+				
+				.record_class {
+					display: flex;
+					flex-wrap: wrap;
+					justify-content: space-between;
+					
+			
+					.record_item {
+						display: flex;
+						flex-direction: row;
+						margin-top: 20rpx;
+						align-items: center;
+						width: 100%;
+						height: auto;
+						margin-bottom: 15rpx;
+						
+						.createTime {
+						  width: 150rpx;
+						  color: #333;
+						  margin-left: 10px;
+						}
+			
+						.image {
+						  width: 260rpx; /* 图像固定宽度 */
+						  height: 175rpx; /* 图像固定高度 */
+						  margin-left: 10px;
+						  align-items: center;
+						}
+			
+						.text {
+						  width: 230rpx;
+						  margin-left: 10px;
+						  align-items: center;
+						}
+					}
+				}
+			}
+			
+		}
+		
+		
+		
+			
+	}
+	
+	
 </style>
